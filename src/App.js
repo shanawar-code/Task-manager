06db0ea41d4a8cf6c59972e10138c4ba2d0ab665
Alim <coder34521@gmail.com>
2024-09-03 18:56:28 +0530

    Changed path

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ import AttendanceAndLeaveHR from "./Pages/HRDashboard/AttandenceAndLeave";
 import FindColleagues from "./Pages/UserDashboard/Chat/FindColleagues";
 import Employeesdetail from "./Pages/HRDashboard/Employees/Employeesdetail";
 import TeamMembers from "./Pages/UserDashboard/MYTeam/TeamMembers";
-import { SignUpPage } from "./Pages/Auth/Signup";
-import { LoginPage } from "./Pages/Auth/Login";
+import { SignUpPage } from "./Pages/Auth/signup";
+import { LoginPage } from "./Pages/Auth/login";
 
 
 
